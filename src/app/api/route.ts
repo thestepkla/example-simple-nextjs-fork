9aa73068b871b230d4cd13a0446d26c95c913b7e
korarit <krtza1@gmail.com>
2025-01-17 01:24:01 +0700

    update

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function GET(request: NextRequest) {
 
     const listMembers = [
-        { studentId: "66024xxx", name: "TestAAA" },
+        { studentId: "66024xxx", name: "TestAAAS" },
         { studentId: "66024xxx", name: "TestCCC" },
         { studentId: "66024xxx", name: "TestBBB" }
     ];
